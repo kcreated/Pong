a83e2655964fd4adb6bfcc5db20d21d8351f5901
kcreated <kyciabt@gmail.com>
2024-05-20 14:58:23 -0400

    Hey!

---
+++ PongBall.java
@@ -0,0 +1,19 @@
+import javax.naming.InitialContext;
+
+public class PongBall 
+extends java.lang.Object{
+
+    private double x;
+    private double y; 
+    private double xVelocity;
+    private double yVelocity;
+  
+    public PongBall(int initX, int initY, int initXVel, int initYVel)
+    x = initX;
+    y = initY;
+
+    xVelocity = initXVel;
+    yVelocity = initYVel;
+
+   
+}
