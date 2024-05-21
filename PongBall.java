032b3a1d7c891a6745394f943a0aa9c9f78227ba
kcreated <kyciabt@gmail.com>
2024-05-21 14:59:52 -0400

    hehugahagh

--- PongBall.java
+++ PongBall.java
@@ -1,19 +1,38 @@
-import javax.naming.InitialContext;
-
 public class PongBall 
 extends java.lang.Object{
 
-    private double x;
-    private double y; 
-    private double xVelocity;
-    private double yVelocity;
+    private int x;
+    private int y; 
+    private int xVelocity;
+    private int yVelocity;
   
-    public PongBall(int initX, int initY, int initXVel, int initYVel)
-    x = initX;
-    y = initY;
+    public PongBall(int initX, int initY, int initXVel, int initYVel){
+        x = initX;
+        y = initY;
+        xVelocity = initXVel;
+        yVelocity = initYVel;
+    }
 
-    xVelocity = initXVel;
-    yVelocity = initYVel;
+    public void bounceX(){
+        xVelocity = -1* xVelocity; 
+    }
 
+    public void bounceY(){
+        yVelocity = -1* yVelocity; 
+
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y; 
+    }
+
+    public void move(){
+        x+=xVelocity;
+        y+=yVelocity;
+    }
    
 }
