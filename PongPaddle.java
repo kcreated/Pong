1feec58542d19f87e26f2c99d93f62d03ed1aeea
kcreated <kyciabt@gmail.com>
2024-05-23 14:58:15 -0400

    AYYAYY :)

--- PongPaddle.java
+++ PongPaddle.java
@@ -1,3 +1,39 @@
-public class PongPaddle {
+public class PongPaddle 
+extends java.lang.Object{
     
+    private int x;
+    private int y;
+    private int width;
+    private int height; 
+
+    public PongPaddle(int top, int left, int w, int h){
+        x = left;
+        y = top;
+        width = w;
+        height = h; 
+    }
+
+    public int getBottomY(){
+        return y+=height;
+    }
+
+    public int getLeftX(){
+        return x;
+    }
+
+    public int getRightX(){
+        return x+=width;
+    }
+
+    public int getTopY(){
+        return y; 
+    }
+
+    public void moveDown(int d){
+        x+=d;
+    }
+
+    public void moveUp(int d){
+        y+=d;
+    }
 }
